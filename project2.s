a3f2161842299798952d77750ed4825e524ef615
Tahj <tahjchisolm21@gmail.com>
2018-11-07 21:35:18 -0500

    get base-N number

--- project2.s
+++ project2.s
@@ -3,12 +3,18 @@
 	
 	la $a0, myWord #stores myWord into a0
 	li $a1, 5 #string has max of 5 characters
+	syscall
 
 	li $v0, 8 #gets the input
 	syscall
 
 
 	li $v0, 10
+	syscall
+
+	div $a0, 10 #gets the last number of the string
+	mfhi $t0
+	add $s0, $t0, 27 #gets the base N and stores into s0
 	syscall
 
 .data
